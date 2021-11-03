7793d7454fa31eedc5f3b0df90d0bdada8a6c3e4
MateuszSzybiak <matszybiak@gmail.com>
2021-11-03 16:12:07 +0100

    pliki lab pep8

--- main.py
+++ main.py
@@ -1,55 +1,49 @@
-#Mateusz Szybiak Informatyka lab2 19.10.2021
+# Mateusz Szybiak Informatyka lab2 19.10.2021
 
 from math import *
-#Zadanie 2
-#a
-"""
-def names(name):
+
+
+# Zadanie 2
+# a
+def names(name) -> None:
     for n in name:
         print(n)
-    return
-names(['ala','kamil','darek','julia','michał'])
-"""
 
-#b
-"""
+
+names(['ala', 'kamil', 'darek', 'julia', 'michał'])
+
+
+# b
+
 def multiply(numbers):
     res = []
     for n in numbers:
-        res.append(2*n)
-    return(res)
+        res.append(2 * n)
+    return res
+
 
 def multiply_list(numbers):
-    res = [2*i for i in numbers]
-    return (res)
+    return [2 * j for j in numbers]
+
 
 print("Sposób 1")
-print("Pierwotna lista: {} \nZwrócona lista: {}".format([1,3,7,22,30], multiply([1,3,7,22,30])))
+print("Pierwotna lista: {} \nZwrócona lista: {}"
+      .format([1, 3, 7, 22, 30], multiply([1, 3, 7, 22, 30])))
 print("Sposób 2")
-print("Pierwotna lista: {} \nZwrócona lista: {}".format([1,3,7,22,30], multiply_list([1,3,7,22,30])))
-"""
+print("Pierwotna lista: {} \nZwrócona lista: {}"
+      .format([1, 3, 7, 22, 30], multiply_list([1, 3, 7, 22, 30])))
 
-#c
-"""
+# c
+
 lista = [1, 2, 5, 10, 13, 14, 18, 21, 24, 30]
 
-for i in range (len(lista)):
+for i in range(len(lista)):
     if lista[i] % 2 == 0:
         print(lista[i])
-"""
 
-#d
-"""
-lista = [1, 2, 5, 10, 13, 14, 18, 21, 24, 30]
+# d
 
-for i in range(floor(len(lista)/2)):
-    print(lista[2*i+1])
-"""
+lista1 = [1, 2, 5, 10, 13, 14, 18, 21, 24, 30]
 
-
-
-
-
-
-
-
+for i in range(floor(len(lista1) / 2)):
+    print(lista1[2 * i + 1])
